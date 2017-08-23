a7d838cd7fde55957ba2f48c1119e52eb4df9f77
Juan Carrizo <juuancarrizo@gmail.com>
2017-08-23 19:38:16 -0300

    Clases del Ejercicio

---
+++ Ejer1/src/Accion.java
@@ -0,0 +1,27 @@
+
+public class Accion {
+	private int idAccion;
+	private String nombre;
+	public Accion() {
+		super();
+	}
+	public Accion(int idAccion, String nombre) {
+		super();
+		this.idAccion = idAccion;
+		this.nombre = nombre;
+	}
+	public int getIdAccion() {
+		return idAccion;
+	}
+	public void setIdAccion(int idAccion) {
+		this.idAccion = idAccion;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
