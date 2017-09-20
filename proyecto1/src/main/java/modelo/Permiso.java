21390528707ce8577b9ec525729a9b4bda51af5a
Unknown <juuancarrizo@gmail.com>
2017-09-20 16:35:24 -0300

    proyecto1
    mapeo prueba

---
+++ proyecto1/src/main/java/modelo/Permiso.java
@@ -0,0 +1,67 @@
+package modelo;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the permiso database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Permiso.findAll", query="SELECT p FROM Permiso p")
+public class Permiso implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private Integer idpermiso;
+
+	//bi-directional many-to-one association to Accion
+	@ManyToOne
+	private Accion accion;
+
+	//bi-directional many-to-one association to Entidad
+	@ManyToOne
+	private Entidad entidad;
+
+	//bi-directional many-to-many association to Rol
+	@ManyToMany(mappedBy="permisos")
+	private List<Rol> rols;
+
+	public Permiso() {
+	}
+
+	public Integer getIdpermiso() {
+		return this.idpermiso;
+	}
+
+	public void setIdpermiso(Integer idpermiso) {
+		this.idpermiso = idpermiso;
+	}
+
+	public Accion getAccion() {
+		return this.accion;
+	}
+
+	public void setAccion(Accion accion) {
+		this.accion = accion;
+	}
+
+	public Entidad getEntidad() {
+		return this.entidad;
+	}
+
+	public void setEntidad(Entidad entidad) {
+		this.entidad = entidad;
+	}
+
+	public List<Rol> getRols() {
+		return this.rols;
+	}
+
+	public void setRols(List<Rol> rols) {
+		this.rols = rols;
+	}
+
+}
